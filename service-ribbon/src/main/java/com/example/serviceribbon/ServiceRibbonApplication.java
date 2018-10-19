1e89d6d987537779df1a32e20f0477c9cc648165
1250211588@qq.com <1250211588@qq.com>
2018-10-19 14:08:09 +0800

    build

--- service-ribbon/src/main/java/com/example/serviceribbon/ServiceRibbonApplication.java
+++ service-ribbon/src/main/java/com/example/serviceribbon/ServiceRibbonApplication.java
@@ -2,11 +2,26 @@ package com.example.serviceribbon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class ServiceRibbonApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceRibbonApplication.class, args);
     }
+
+    @Bean
+    //表明这个restRemplate开启负载均衡的功能。
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
 }
